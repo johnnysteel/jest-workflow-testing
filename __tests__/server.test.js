2779ea3c0090befde65b3ea283298483ade00461
jnbissu589@gmail.com <jnbissu589@gmail.com>
2024-07-25 16:15:41 -0400

    first commit

---
+++ __tests__/server.test.js
@@ -0,0 +1,21 @@
+// __tests__/server.test.js
+const request = require('supertest');
+const app = require('../server/index');
+
+let server;
+
+beforeEach((done) => {
+  server = app.listen(3001, done);  // Use a different port to avoid conflict
+});
+
+afterEach((done) => {
+  server.close(done);
+});
+
+describe('GET /', () => {
+  it('should return Hello World', async () => {
+    const res = await request(server).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello World!');
+  });
+});
