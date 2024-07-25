7240b6562ab5a4ed6990f9477312da9507b04091
jnbissu589@gmail.com <jnbissu589@gmail.com>
2024-07-25 16:30:44 -0400

    change hello world to hello worlds

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -16,6 +16,6 @@ describe('GET /', () => {
   it('should return Hello World', async () => {
     const res = await request(server).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello World!');
+    expect(res.text).toBe('Hello Worlds!');
   });
 });
